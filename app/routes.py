31e2d710575f96b4408b4a071b0b022aa5154fae
Andrea Palacios <andreapalacios88@gmail.com>
2021-04-30 12:02:32 -0700

    created 3 endpoints and a model as well as database migration

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
 from flask import Blueprint
 
+
+solar_system_bp = Blueprint("solar_system", __name__)
+
+@solar_system_bp.route('/planets', methods = ["GET"])
+
+def get_planets():
+    my_response = "planets"
+    return my_response
+    
+@solar_system_bp.route('/planet', methods = ["GET"])
+
+def get_planet():
+    my_response = "planet"
+    return my_response
+
+@solar_system_bp.route('/new_planet', methods = ["POST"])
+
+def get_new_planet():
+    my_response = "new planet"
+    return my_response
