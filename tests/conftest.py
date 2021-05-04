f42ed843a72e53c0b9c40a305133629c10f3ce3a
Katrina Li <likatrina7@gmail.com>
2021-05-04 16:17:11 -0700

    Create more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from app import create_app
 from app import db
-
+from app.Models.planet import Planet
 
 @pytest.fixture
 def app():
@@ -18,3 +18,22 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    mars = Planet(name="Mars",
+                    description="Red planet")
+    venus = Planet(name="Venus",
+                    description="Large planet")
+
+    db.session.add_all([mars, venus])
+    db.session.commit()
+
+@pytest.fixture
+def planet_data(app):
+    # Arrange
+    return {
+        "name": "Sun",
+        "description": "Very hot"
+    }
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,54 @@
-def test_get_all_planets(client):
+def test_get_all_planets_no_records(client):
     # Act
     response = client.get("/planets")
     response_body = response.get_json()
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_all_planets_with_records(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "name": "Mars",
+        "description": "Red planet"
+    },{
+        "id": 2,
+        "name": "Venus",
+        "description": "Large planet"
+    }]
+
+def test_get_single_planet_with_records(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Mars",
+        "description": "Red planet"
+    }
+
+def test_get_single_planet_without_records(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 404
+    # assert response_body == {
+    #         "success": False,
+    #         "message": f"Planet id_1 was not found"
+    #     }
+
+def test_create_one_planet(client, planet_data):
+    # Act
+    response = client.post("/planets", json=planet_data)
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 201
+
